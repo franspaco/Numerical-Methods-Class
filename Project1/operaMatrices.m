e5eb10ccb742559ac4d6c60ac9704199da30a592
franspaco <franspaco@gmail.com>
2016-08-29 14:51:19 -0500

    Analisis y MaxMin

--- Project1/operaMatrices.m
+++ Project1/operaMatrices.m
@@ -12,29 +12,35 @@ function [R, E, M] = operaMatrices(A, B, N)
   case 3
     [R, E, M] = analizarMatrices(A, B);
   case 4
-    [R, E, M] = sumarMatrices(A, B);
+    [R, E, M] = maxMinMatrices(A, B);
+  otherwise
+    R = 0;
+    E = 1;
+    M = 'Opcion NO existe!';
   endswitch
 end
 
 function [R, E, M] = sumarMatrices(A, B, N)
-  if size(A) ~= size(B)
+  if (size(A)(1) ~= size(B)(1)) || (size(A)(2) ~= size(B)(2))
     E = 1;
     R = 0;
-    if N==1
+    if N==0
       M = 'Las dimensiones de las matrices impiden su suma.';
-    elseif N==2
+    elseif N==1
       M = 'Las dimensiones de las matrices impiden su resta.';
     end
   else
     E = 0;
-    M = 0;
+    M = 'No hay error.';
+    D = 0
+    if N==0
+      D = 1;
+    elseif N==1
+      D = -1;
+    end
     for j = 1:size(A)(1)
       for k = 1:size(A)(2)
-        if N==0
-          R(j,k) = A(j,k) + B(j,k);
-        elseif N==1
-          R(j,k) = A(j,k) - B(j,k);
-        end
+        R(j,k) = A(j,k) + (D*B(j,k));
       end
     end
   end
@@ -47,7 +53,7 @@ function [R, E, M] = multiplicarMatrices(A, B, N)
     R = 0;
   else
     E = 0;
-    M = 0;
+    M = 'No hay error.';
     for j=1:size(A)(1)
       for k=1:size(B)(2)
         R(j,k) = 0;
@@ -56,5 +62,44 @@ function [R, E, M] = multiplicarMatrices(A, B, N)
         end
       end
     end
+  end
+end
+
+function [R, E, M] = analizarMatrices(A)
+  for j = 1:size(A)(1)
+    R(j,1) = 0;
+    R(j,2) = 0;
+    columns = size(A)(2);
+    for k = 1:columns
+      R(j,1) += A(j,k);
+    end
+    R(j,1) = R(j,1)/columns;
+
+    for k = 1:columns
+      R(j,2) += (A(j,k)-R(j,1))**2;
+    end
+    R(j,2) = R(j,2)/(columns-1);
+    R(j,2) = sqrt(R(j,2));
+  end
+  E = 0;
+  M = 'No hay error.';
+end
+
+function [R, E, M] = maxMinMatrices(A)
+  for j = 1:size(A)(2)
+    max = A(1,j);
+    min = max;
+    for k = 1:size(A)(1)
+      if A(k,j) > max
+        max = A(k,j);
+      end
+      if A(k,j) < min
+        min = A(k,j);
+      end
+    end
+    R(1,j) = max;
+    R(2,j) = min;
+    E = 0;
+    M = 'No hay error.';
   end
 end
